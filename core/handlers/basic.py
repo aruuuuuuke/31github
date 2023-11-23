cd51241e1c1e3bf2ff55f449ffe2f5d72ff2aa2d
aruuuuuuke <askatovaaruuke657@gmail.com>
2023-11-23 20:53:21 +0300

    first commit

---
+++ core/handlers/basic.py
@@ -0,0 +1,7 @@
+from aiogram import Bot
+from aiogram.types import Message
+
+async def get_start(message: Message, bot: Bot):
+    await bot.send_message(message.from_user.id, f'<b> привеет {message.from_user.first_name}. Рад тебя видеть!</b>')
+    await message.answer(f'<s>привеет {message.from_user.first_name}. Рад тебя видеть!</s>')
+    await message.reply(f'<tg-spoiler>привеет {message.from_user.first_name}. Рад тебя видеть!</tg-spoiler>')
---
+++ main.py
@@ -0,0 +1,39 @@
+import asyncio
+import logging
+from aiogram.types import Message
+from aiogram import Bot, Dispatcher
+
+token = '6560684648:AAE-Z17JMWefoiohcgujZu9KaL5WK0y3kpI'
+
+
+async def start_bot(bot: Bot):
+    await bot.send_message(5553751043, text='Бот запущен')
+
+
+async def stop_bot(bot: Bot):
+    await bot.send_message(5553751043, text='Бот остановлен')
+
+
+async def get_start(message: Message, bot: Bot):
+    await bot.send_message(message.from_user.id, f'<b> привеет {message.from_user.first_name}. Рад тебя видеть!</b>')
+    await message.answer(f'<s>привеет {message.from_user.first_name}. Рад тебя видеть!</s>')
+    await message.reply(f'<tg-spoiler>привеет {message.from_user.first_name}. Рад тебя видеть!</tg-spoiler>')
+
+
+async def start():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=token, parse_mode='HTML')
+
+    dp = Dispatcher()
+
+    dp.startup.register(start_bot)
+    dp.shutdown.register(stop_bot)
+    dp.message.register(get_start)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(start())
