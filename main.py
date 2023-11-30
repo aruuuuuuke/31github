c0d6de308b43290ed1e15fd1f4e714a48a983c14
aruuuuuuke <askatovaaruuke657@gmail.com>
2023-11-30 14:07:22 +0300

    keyboard!

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ async def stop_bot(bot: Bot):
     await bot.send_message(settings.bots.admin_id, text='Бот остановлен')
 
 
-async def get_start(message: Message, bot: Bot):
-    await bot.send_message(message.from_user.id, f'<b> привеет {message.from_user.first_name}. Рад тебя видеть!</b>')
-    await message.answer(f'<s>привеет {message.from_user.first_name}. Рад тебя видеть!</s>')
-    await message.reply(f'<tg-spoiler>привеет {message.from_user.first_name}. Рад тебя видеть!</tg-spoiler>')
+
+#async def get_start(message: Message, bot: Bot):
+ #   await bot.send_message(message.from_user.id, f'<b> привеет {message.from_user.first_name}. Рад тебя видеть!</b>')
+  #  await message.answer(f'<s>привеет {message.from_user.first_name}. Рад тебя видеть!</s>')
+   # await message.reply(f'<tg-spoiler>привеет {message.from_user.first_name}. Рад тебя видеть!</tg-spoiler>')
 
 
 async def start():
